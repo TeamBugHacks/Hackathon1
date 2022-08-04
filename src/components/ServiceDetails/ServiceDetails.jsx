d9efaade8606c9f80915fc777f3147a37d0e9ebe
Aadarsh Thakur <aadarshthakur80501@gmail.com>
2022-08-04 23:22:59 +0530

    service details page

--- src/components/ServiceDetails/ServiceDetails.jsx
+++ src/components/ServiceDetails/ServiceDetails.jsx
@@ -1,9 +1,81 @@
-import {useParams} from 'react-router-dom'
+import WebIcon from "@mui/icons-material/Web";
 
 const ServiceDetails = () => {
   return (
-    <div>ServiceDetails</div>
-  )
-}
+    <div className="serviceDetails">
+      <nav className="nav">
+        <div className="logo">Web</div>
+      </nav>
+      <hr />
 
-export default ServiceDetails
+      <main className="main">
+        <div className="main__content">
+          <div className="chip">
+            <WebIcon />
+            Web
+          </div>
+          <h2 className="main__title">Web services at its peek.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
+            cumque possimus nisi.
+          </p>
+        </div>
+
+        <main className="img">
+          <img
+            src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80"
+            alt=""
+          />
+        </main>
+      </main>
+
+      <hr />
+
+      <div className="services__sublist">
+        <div className="sublist__item">
+          <img
+            src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_64,dpr_1,h_64,fl_progressive:steep,q_auto/images/growth/luminosity/1646814042922-25f8f1.jpeg"
+            alt=""
+            className="listItem__img"
+          />
+          <div className="listItem__title">Frontend</div>
+        </div>
+        <div className="sublist__item">
+          <img
+            src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_64,dpr_1,h_64,fl_progressive:steep,q_auto/images/growth/luminosity/1646814042922-25f8f1.jpeg"
+            alt=""
+            className="listItem__img"
+          />
+          <div className="listItem__title">Backend</div>
+        </div>
+        <div className="sublist__item">
+          <img
+            src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_64,dpr_1,h_64,fl_progressive:steep,q_auto/images/growth/luminosity/1646814042922-25f8f1.jpeg"
+            alt=""
+            className="listItem__img"
+          />
+          <div className="listItem__title">Devops</div>
+        </div>
+        <div className="sublist__item">
+          <img
+            src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_64,dpr_1,h_64,fl_progressive:steep,q_auto/images/growth/luminosity/1646814042922-25f8f1.jpeg"
+            alt=""
+            className="listItem__img"
+          />
+          <div className="listItem__title">Api development</div>
+        </div>
+        <div className="sublist__item">
+          <img
+            src="https://res.cloudinary.com/urbanclap/image/upload/t_high_res_template,q_auto:low,f_auto,w_64,dpr_1,h_64,fl_progressive:steep,q_auto/images/growth/luminosity/1646814042922-25f8f1.jpeg"
+            alt=""
+            className="listItem__img"
+          />
+          <div className="listItem__title">DBMS</div>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ServiceDetails;
